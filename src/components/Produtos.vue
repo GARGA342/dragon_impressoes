8d0a3804c3a2e0115c497874f8b4c44a1e2d3bea
Unipampa <unipampa@pop-os.localdomain>
2023-10-12 07:43:07 -0300

    Updating products card

--- src/components/Produtos.vue
+++ src/components/Produtos.vue
@@ -1,28 +1,42 @@
 <template>
     <section class="main" id="produtos">
         <h1>Produtos</h1>
-        <div class="content">
-            <div class="indicator"><img src="../assets/img/left_arrow.png" class="arrow"></div>
+        <div class="gallery">
+           <img src="../assets/img/left_arrow.png" class="arrow prev">
+            
+            <div class="content">
+                <div class="card">
+                    <img src="../assets/img/cards/1.jpg" class="card_img" alt="">
+                    <p class="card_p">Porta copos de madeira</p>
+                    <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                </div>
 
-            <div class="card">
-                <img src="../assets/img/cards/1.jpg" class="card_img" alt="">
-                <p class="card_p">Porta copos de madeira</p>
-                <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                <div class="card">
+                    <img src="../assets/img/cards/2.jpg" class="card_img" alt="">
+                    <p class="card_p">Artigos para decoração</p>
+                    <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                </div>
+
+                <div class="card active">
+                    <img src="../assets/img/cards/3.jpg" class="card_img" alt="">
+                    <p class="card_p">Porta copos em PLA</p>
+                    <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                </div>
+
+                <div class="card">
+                    <img src="../assets/img/cards/4.jpg" class="card_img" alt="">
+                    <p class="card_p">Acessórios em PLA</p>
+                    <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                </div>
+
+                <div class="card">
+                    <img src="../assets/img/cards/4.jpg" class="card_img" alt="">
+                    <p class="card_p">Acessórios em PLA</p>
+                    <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
+                </div>
             </div>
 
-            <div class="card">
-                <img src="../assets/img/cards/2.jpg" class="card_img" alt="">
-                <p class="card_p">Artigos para decoração</p>
-                <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
-            </div>
-
-            <div class="card">
-                <img src="../assets/img/cards/3.jpg" class="card_img" alt="">
-                <p class="card_p">Porta copos em PLA</p>
-                <button class="btn_pedido"><span>FAÇA SEU PEDIDO</span></button>
-            </div>
-
-            <div class="indicator"><img src="../assets/img/right_arrow.png" class="arrow"></div>
+            <img src="../assets/img/right_arrow.png" class="arrow next">
         </div>
     </section>
 </template>
@@ -31,7 +45,7 @@
     .main{
         background-color: #C50018;
         padding: auto 0 auto;
-        min-height: 100vh;
+        min-height: 80vh;
     }
 
     h1{
@@ -41,12 +55,29 @@
     }
 
     .content{
-        margin-top: 1.5rem;
-        height: 25rem;
-        background: rgba(132, 0, 16, 0.28);
         display: flex;
         justify-content: space-around;
         align-items: center;
+        overflow: auto;
+        gap: 100px;
+        max-width: 1200px;
+        margin-inline: auto;
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
+
+    /* Hide scrollbar for Chrome, Safari and Opera */
+    .content::-webkit-scrollbar {
+        display: none;
+    }
+
+    .gallery{
+        margin-top: 2.3rem;
+        background: rgba(132, 0, 16, 0.28);
+        height: 25rem;
+        display: flex;
+        align-items: center;
+        padding-inline: 2.3rem;
     }
 
     .card{
@@ -75,6 +106,7 @@
     .arrow{
         height: 3rem;
         align-self: center;
+        cursor: pointer;
     }
 
     .btn_pedido{
@@ -93,8 +125,36 @@
         color: white;
         font-weight: 800;
         font-family: Inter;
-    }   
+    }
 </style>
 
 <script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+    const controls = document.querySelectorAll('.arrow')
+    const items = document.querySelectorAll('.card')
+    let current_item = 0
+    const max_items = items.length
+
+    controls.forEach(control => {
+        control.addEventListener('click', () =>{
+            const prev = control.classList.contains('prev')
+            if(prev){
+                current_item -= 1
+            }else{
+                current_item += 1
+            }
+
+            if(current_item >= max_items){
+                current_item = 0
+            }
+
+            if(current_item < 0){
+                current_item = max_items - 1
+            }
+
+        })
+    })
+})
 </script>
