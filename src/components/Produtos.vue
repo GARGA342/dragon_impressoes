b386e4e5015e656aaefeb9a94dd3f8118996840b
Gabriel Prates da Costa <gabriel.prates.costa@gmail.com>
2023-11-04 20:17:47 -0300

    Updated carousel responsiveness

--- src/components/Produtos.vue
+++ src/components/Produtos.vue
@@ -10,18 +10,12 @@
         :gap="3"
         :slide-ratio="1 / 4"
         :dragging-distance="200"
-        :breakpoints="{ 800: { visibleSlides: 2, slideMultiple: 2 } }"
+        :breakpoints="{ 975: { visibleSlides: 2, slideMultiple: 2 } }"
       >
-        <vueper-slide
-          class="card"
-          v-for="(card, i) in cards"
-          :key="i"
-        >
+        <vueper-slide class="card" v-for="(card, i) in cards" :key="i">
           <template #content>
-            <div
-              class="vueperslide__content-wrapper"
-            >
-              <img :src="card.img" class="card_img">
+            <div class="vueperslide__content-wrapper">
+              <img :src="card.img" class="card_img" />
               <p>{{ card.title }}</p>
               <button><span>FAÇA SEU PEDIDO</span></button>
             </div>
@@ -66,69 +60,6 @@ export default {
   background-color: white;
 }
 
-.card_img{
-  max-width: 80%;
-  border-radius: 5%;
-  display: block;
-  margin-inline: auto;
-  box-shadow: #484848 5px 5px 20px 1px;
-}
-
-.vueperslide__content-wrapper button{
-  display: block;
-  border-radius: 0.8rem;
-  background: #c50018;
-  width: 90%;
-  margin-inline: auto;
-  margin-top: 15px;
-  padding: 0.5rem;
-  border: none;
-  cursor: pointer;
-}
-
-.vueperslide__content-wrapper button span{
-  color: white;
-  font-weight: 800;
-  font-family: Inter;
-}
-
-.vueperslide__content-wrapper{
-  display: block !important;
-  padding-top: 10px;
-  text-align: unset !important;
-}
-
-.vueperslide__content-wrapper p{
-  padding: 1rem 0 0 2rem;
-}
-
-.vueperslides__parallax-wrapper{
-  padding-bottom: calc(120px + 20%) !important;
-}
-
-.vueperslides__bullet .default {
-  background-color: rgba(0, 0, 0, 0.3);
-  border: none;
-  box-shadow: none;
-  transition: 0.3s;
-  width: .7rem;
-  height: .7rem;
-  transition: .5s;
-}
-
-.vueperslides__bullet--active .default {
-  background-color: white;
-}
-
-.vueperslides__bullet span {
-  display: block;
-  color: #fff;
-  font-size: 10px;
-  opacity: 0.8;
-}
-
-.vueperslides__arrow {color: white}
-
 .main_product {
   background-color: #c50018;
   padding: 50px 0;
@@ -144,5 +75,76 @@ h1 {
   color: white;
   font-weight: bold;
   margin-inline: 4.69rem;
+}
+
+.card_img {
+  max-width: 80%;
+  border-radius: 5%;
+  display: block;
+  margin-inline: auto;
+  box-shadow: #484848 5px 5px 20px 1px;
+}
+
+.vueperslide__content-wrapper button {
+  display: block;
+  border-radius: 0.8rem;
+  background: #c50018;
+  width: 90%;
+  margin-inline: auto;
+  margin-top: 15px;
+  padding: 0.5rem;
+  border: none;
+  cursor: pointer;
+}
+
+.vueperslide__content-wrapper button span {
+  color: white;
+  font-weight: 800;
+  font-family: Inter;
+}
+
+.vueperslide__content-wrapper {
+  display: block !important;
+  padding-top: 10px;
+  text-align: unset !important;
+}
+
+.vueperslide__content-wrapper p {
+  padding: 1rem 0 0 2rem;
+}
+
+.vueperslides__parallax-wrapper {
+  padding-bottom: calc(120px + 20%) !important;
+}
+
+.vueperslides__bullet .default {
+  background-color: rgba(0, 0, 0, 0.3);
+  border: none;
+  box-shadow: none;
+  transition: 0.3s;
+  width: 0.7rem;
+  height: 0.7rem;
+  transition: 0.5s;
+}
+
+.vueperslides__bullet--active .default {
+  background-color: white;
+}
+
+.vueperslides__bullet span {
+  display: block;
+  color: #fff;
+  font-size: 10px;
+  opacity: 0.8;
+}
+
+.vueperslides__arrow {
+  color: white;
+}
+
+@media (max-width: 975px){
+  .vueperslides__parallax-wrapper {
+    padding-bottom: calc(120px + 32%) !important;
+  }
 }
 </style>
