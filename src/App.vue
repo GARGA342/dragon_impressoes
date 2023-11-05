a28134972557381203bbd7c05eb848c647c2530d
Gabriel Prates da Costa <gabriel.prates.costa@gmail.com>
2023-11-05 07:06:37 -0300

    Adjusted navigation

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,43 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import Home from './components/Home.vue'
-import Sobre from './components/Sobre.vue'
-import Produtos from './components/Produtos.vue'
-import Pedidos from './components/Pedidos.vue'
-import Rodape from './components/Rodape.vue'
+import Navbar from "./components/Navbar.vue";
+import Home from "./components/Home.vue";
+import Sobre from "./components/Sobre.vue";
+import Produtos from "./components/Produtos.vue";
+import Pedidos from "./components/Pedidos.vue";
+import Rodape from "./components/Rodape.vue";
+
+function scrollEvent(id){
+ const section = document.querySelector(id);
+ if (section) {
+   section.scrollIntoView({
+     behavior: "smooth",
+     block: "start",
+   });
+ }
+}
 </script>
 
 <template>
-  <Navbar />
-  <Home />
-  <img id="home_div" src="./assets/img/white_div.png">
-  <Sobre />
-  <img id="about_div" src="./assets/img/red_div.png">
-  <Produtos />
-  <Pedidos id="pedidos"/>
-  <Rodape />
+ <Navbar v-on:scroll-event="scrollEvent" />
+ <Home />
+ <img id="home_div" src="./assets/img/white_div.png" />
+ <Sobre />
+ <img id="about_div" src="./assets/img/red_div.png" />
+ <Produtos />
+ <Pedidos id="pedidos" />
+ <Rodape />
 </template>
 
 <style>
-  #home_div{
-    display: block;
-    width: 100%;
-    background-color: #484848;
-  }
+#home_div {
+  display: block;
+  width: 100%;
+  background-color: #484848;
+}
 
-  #about_div{
-    display: block;
-    width: 100%;
-    background-color: white;
-  }
+#about_div {
+  display: block;
+  width: 100%;
+  background-color: white;
+}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,61 +1,58 @@
 <template>
-    <div class="nav_body">
-        <a @click="scrollToId('#inicio')"><img src='../assets/img/nav_logo.png' class="nav_logo" alt="Logo da empresa"/></a>
-        <nav class="nav_links">
-            <a @click="scrollToId('#inicio')">Inicio</a>
-            <a @click="scrollToId('#home_div')">Sobre</a>
-            <a @click="scrollToId('#about_div')">Produtos</a>
-            <a @click="scrollToId('#pedidos')">Pedidos</a>
-        </nav>
-    </div>
+  <div class="nav_body">
+    <a @click="scrollEvent('#inicio')"
+      ><img src="../assets/img/nav_logo.png" class="nav_logo" alt="Logo da empresa"
+    /></a>
+    <nav class="nav_links">
+      <a @click="scrollEvent('#inicio')">Inicio</a>
+      <a @click="scrollEvent('#home_div')">Sobre</a>
+      <a @click="scrollEvent('#about_div')">Produtos</a>
+      <a @click="scrollEvent('#pedidos')">Pedidos</a>
+    </nav>
+  </div>
 </template>
 
 <script setup>
-function scrollToId(id){
-  const section = document.querySelector(id);
-  if (section) {
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  }
+const emit = defineEmits(["scrollEvent"]);
+
+function scrollEvent(id) {
+  emit("scrollEvent", id);
 }
 </script>
 
-
 <style scoped>
-.nav_body{
-    position: fixed;
-    background-color: #C50018;
-    width: 100%;
-    height: 4em;
-    display: flex;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-    align-items: center;
-    justify-content: space-between;
-    z-index: 1;
+.nav_body {
+  position: fixed;
+  background-color: #c50018;
+  width: 100%;
+  height: 4em;
+  display: flex;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  align-items: center;
+  justify-content: space-between;
+  z-index: 1;
 }
 
-.nav_links{
-    display: flex;
-    margin-right: 20px;
-    gap: 10px;
-    cursor: pointer;
+.nav_links {
+  display: flex;
+  margin-right: 20px;
+  gap: 10px;
+  cursor: pointer;
 }
 
-.nav_links a{
-    text-decoration: none;
-    color: white;
-    font-family: Inter;
+.nav_links a {
+  text-decoration: none;
+  color: white;
+  font-family: Inter;
 }
 
-.nav_links a:hover{
-    color:#FFAE03;
+.nav_links a:hover {
+  color: #ffae03;
 }
 
 .nav_logo {
-    padding: 10px;
-    width: 50px;
-    cursor: pointer;
+  padding: 10px;
+  width: 50px;
+  cursor: pointer;
 }
 </style>
